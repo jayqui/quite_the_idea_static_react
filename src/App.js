520eda381e606cd3b33b91a13798eca0747a4e9c
Jay Quigley <quigley.james.g@gmail.com>
2019-02-07 15:03:23 -0800

    replicate static version of the site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import data from './data/data.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div class="logo-container">
+            <img class="logo" src={logo} />
+          </div>
+          <div class="music-links-container">
+            <ul class="music-links-ul">
+              {data.map(datum =>
+                <li class="music-links-li">
+                  <a
+                    target="_blank"
+                    className={`music-link ${datum.className}`}
+                    href={datum.href}
+                  >
+                    <img
+                      // src={datum.src}
+                      // src={spotify}
+                      src={datum.logo}
+                      title={datum.title}
+                      alt={datum.alt}
+                    />
+                  </a>
+                </li>
+              )}
+            </ul>
+          </div>
         </header>
       </div>
     );
