d17ea383e3d6358d5a9be2f07b0a40f584d98d2c
Jay Quigley <quigley.james.g@gmail.com>
2019-02-07 23:32:00 -0800

    Componentize
    Have `<App />` pass data to the new dumb components `<Logo />`, `<Footer />`, and `<FooterItem />`.

--- src/App.js
+++ src/App.js
@@ -1,37 +1,19 @@
 import React, { Component } from 'react';
+import Footer from './Footer';
+import Logo from './Logo';
+
 import logo from './logo.svg';
-import './App.css';
 import data from './data/data.js';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <div class="logo-container">
-            <img class="logo" src={logo} />
-          </div>
-          <div class="music-links-container">
-            <ul class="music-links-ul">
-              {data.map(datum =>
-                <li class="music-links-li">
-                  <a
-                    target="_blank"
-                    className={`music-link ${datum.className}`}
-                    href={datum.href}
-                  >
-                    <img
-                      // src={datum.src}
-                      // src={spotify}
-                      src={datum.logo}
-                      title={datum.title}
-                      alt={datum.alt}
-                    />
-                  </a>
-                </li>
-              )}
-            </ul>
-          </div>
+          <Logo logo={logo} />
+          <Footer data={data} />
         </header>
       </div>
     );
