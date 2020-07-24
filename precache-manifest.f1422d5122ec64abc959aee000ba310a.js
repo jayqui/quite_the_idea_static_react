ae737293c7cd8a88cbfa38ae6d2d2eb664241021
Jay Quigley <quigley.james.g@gmail.com>
2020-07-24 13:00:51 -0700

    Updates

---
+++ precache-manifest.f1422d5122ec64abc959aee000ba310a.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ec540be6e687dad3286",
+    "url": "/static/css/main.8fcaa99c.chunk.css"
+  },
+  {
+    "revision": "0ec540be6e687dad3286",
+    "url": "/static/js/main.0ec540be.chunk.js"
+  },
+  {
+    "revision": "63e765c8434c8f214054",
+    "url": "/static/js/1.63e765c8.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23513deaedfb19234ec4538c64463aef",
+    "url": "/static/media/logo.23513dea.svg"
+  },
+  {
+    "revision": "d7397bd6d02672c97e3e4f3d76ab20c1",
+    "url": "/static/media/spotify.d7397bd6.png"
+  },
+  {
+    "revision": "a186227d5591bb93d3d97d1153583dba",
+    "url": "/static/media/soundcloud.a186227d.png"
+  },
+  {
+    "revision": "9de56b736a76bd1b936f2a5a262ff141",
+    "url": "/static/media/bandcamp.9de56b73.png"
+  },
+  {
+    "revision": "017e87d34e3cf2bec0d7a18498c40579",
+    "url": "/static/media/googleplay.017e87d3.png"
+  },
+  {
+    "revision": "5e5bcf642ab8e039d24faa92548cd09f",
+    "url": "/static/media/apple.5e5bcf64.png"
+  },
+  {
+    "revision": "18a6a65e73272394565f2e12cf8a4bb3",
+    "url": "/static/media/amazon.18a6a65e.png"
+  },
+  {
+    "revision": "e5775251f854b3040d5a5de04d3bc709",
+    "url": "/static/media/napster.e5775251.png"
+  },
+  {
+    "revision": "db23cb54ecf742d1d002f9d812915780",
+    "url": "/static/media/tidal.db23cb54.png"
+  },
+  {
+    "revision": "c974a9cc313f8161d5254c4b6fbbf889",
+    "url": "/static/media/deezer.c974a9cc.png"
+  },
+  {
+    "revision": "71e5c11d1b4f9f63fd68fe4006978884",
+    "url": "/static/media/eyes_cityscape_cropped.71e5c11d.jpg"
+  },
+  {
+    "revision": "9532bd11187c15d92a376a49a7783f91",
+    "url": "/index.html"
+  }
+];
