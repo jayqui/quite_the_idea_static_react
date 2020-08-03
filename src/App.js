4d131cc24b677f5f567f0328c12a82c116addb2f
Jay Quigley <quigley.james.g@gmail.com>
2020-08-02 19:29:16 -0700

    add react-router-dom with play-around 'bar' route

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import Footer from './Footer';
 import Logo from './Logo';
 
@@ -7,12 +8,27 @@ import data from './data/data.js';
 
 import './App.css';
 
+const NoMatchPage = () => {
+  return (
+    <h3>404 - Not found</h3>
+  );
+};
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <Logo logo={logo} />
+          <BrowserRouter>
+            <Switch>
+              <Route path='/bar'>
+                bar
+              </Route>
+              <Route exact path='/'></Route>
+              <Route component={NoMatchPage} />
+            </Switch>
+          </BrowserRouter>
           <Footer data={data} />
         </header>
       </div>
