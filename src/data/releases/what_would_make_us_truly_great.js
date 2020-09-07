e681dadbb39736367d35277dfc20371278027588
Jay Quigley <quigley.james.g@gmail.com>
2020-09-07 08:25:15 -0700

    Further links for "What Would...": Amazon, iHeart, Napster, Tidal

--- src/data/releases/what_would_make_us_truly_great.js
+++ src/data/releases/what_would_make_us_truly_great.js
@@ -6,10 +6,10 @@ const whatWouldMakeUsTrulyGreat = {
   apple: 'https://music.apple.com/us/album/what-would-make-us-truly-great-single/1527376468',
   google: 'https://play.google.com/store/music/album/Quite_the_Idea_What_Would_Make_Us_Truly_Great?id=Blekwsqnwupq2tcamiufurgr2la',
   deezer: 'https://www.deezer.com/us/album/166610562',
-  iheart: '',
-  napster: '',
-  tidal: '',
-  amazon: '',
+  iheart: 'https://www.iheart.com/artist/quite-the-idea-32295402/albums/what-would-make-us-truly-great-108964234/',
+  napster: 'https://us.napster.com/artist/quite-the-idea/album/what-would-make-us-truly-great-single',
+  tidal: 'https://www.tidal.com/browse/track/151782939',
+  amazon: 'https://music.amazon.com/albums/B08FR43PKS',
 };
 
 export default whatWouldMakeUsTrulyGreat;
